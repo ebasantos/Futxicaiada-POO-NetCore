579db01239b4c009d3912790575f92a171b9a500
Glauber Atanakados Santos <glauberatanaka@gmail.com>
2019-11-15 23:04:11 -0400

    Initial commit

---
+++ WebZoo/Controllers/AnimalsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebZoo.Data;
+using WebZoo.Models.Zoo;
+
+namespace WebZoo.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private readonly ZooContext _context;
+
+        public AnimalsController(ZooContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Animals
+        public async Task<IActionResult> Index()
+        {
+            var zooContext = _context.Animais.Include(a => a.Especie);
+            return View(await zooContext.ToListAsync());
+        }
+
+        // GET: Animals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animais
+                .Include(a => a.Especie)
+                .FirstOrDefaultAsync(m => m.AnimalId == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        // GET: Animals/Create
+        public IActionResult Create()
+        {
+            ViewData["EspecieId"] = new SelectList(_context.Especies, "EspecieId", "Nome");
+            return View();
+        }
+
+        // POST: Animals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AnimalId,Nome,EspecieId")] Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(animal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EspecieId"] = new SelectList(_context.Especies, "EspecieId", "Nome", animal.EspecieId);
+            return View(animal);
+        }
+
+        // GET: Animals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animais.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            ViewData["EspecieId"] = new SelectList(_context.Especies, "EspecieId", "Nome", animal.EspecieId);
+            return View(animal);
+        }
+
+        // POST: Animals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AnimalId,Nome,EspecieId,DataCadastro")] Animal animal)
+        {
+            if (id != animal.AnimalId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(animal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnimalExists(animal.AnimalId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EspecieId"] = new SelectList(_context.Especies, "EspecieId", "Nome", animal.EspecieId);
+            return View(animal);
+        }
+
+        // GET: Animals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animais
+                .Include(a => a.Especie)
+                .FirstOrDefaultAsync(m => m.AnimalId == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return View(animal);
+        }
+
+        // POST: Animals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var animal = await _context.Animais.FindAsync(id);
+            _context.Animais.Remove(animal);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return _context.Animais.Any(e => e.AnimalId == id);
+        }
+    }
+}
---
+++ WebZoo/Models/Zoo/Animal.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebZoo.Models.Zoo
+{
+    public class Animal
+    {
+        [Key]
+        public int AnimalId { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [ForeignKey("Especie")]
+        public int EspecieId { get; set; }
+        [Required]
+        [HiddenInput(DisplayValue = true)]
+        [Display(Name = "Cadastro")]
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+        
+        public virtual Especie Especie { get; set; }
+    }
+}
